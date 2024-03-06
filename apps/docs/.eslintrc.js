3b7b134c48ffb56b6c1c300fdc51f82921eda16e
JayeshVP24 <jayeshpotlabattini@gmail.com>
2024-03-06 13:00:25 +0530

    chore: removed templated docs app

--- apps/docs/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["@projekt906/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
